6e075ab2543c907732fb48fa7012f04d1369823a
radhaya9av <radhaya9av@gmail.com>
2024-07-28 22:38:53 +0530

    minorFixes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { useSidebarContext } from '../../context/sidebarContext';
-import { ImCancelCircle} from "react-icons/im";
+import { ImCancelCircle } from "react-icons/im";
 import "./Sidebar.scss";
 import { Link } from "react-router-dom";
 import { useMealContext } from '../../context/mealContext';
 
 const Sidebar = () => {
-    const { isSidebarOpen, closeSidebar} = useSidebarContext();
+    const { isSidebarOpen, closeSidebar } = useSidebarContext();
     const { categories } = useMealContext();
 
     return (
         <nav className={`sidebar ${isSidebarOpen ? 'sidebar-visible' : ""}`}>
-            <button type = "button" className='navbar-hide-btn' onClick={() => closeSidebar()}>
-                <ImCancelCircle size = {24} />
+            <button type="button" className='navbar-hide-btn' onClick={() => closeSidebar()}>
+                <ImCancelCircle size={24} />
             </button>
 
             <div className='side-content'>
                 <ul className='side-nav'>
                     {
-                        categories.map(category => (
-                            <li className='side-item' key = {category.idCategory}>
-                                <Link to = {`/meal/category/${category.strCategory}`} className='side-link ls-1 fs-13' onClick={() => closeSidebar()}>
+                        categories ? categories.map(category => (
+                            <li className='side-item' key={category.idCategory}>
+                                <Link to={`/meal/category/${category.strCategory}`} className='side-link ls-1 fs-13' onClick={() => closeSidebar()}>
                                     {category.strCategory}
                                 </Link>
                             </li>
-                        ))
+                        )) : <li>Loading...</li>
                     }
                 </ul>
             </div>
