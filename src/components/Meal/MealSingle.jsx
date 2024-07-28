35698786c64d3f0c7d2a2f65be488d1741ea7d16
radhaya9av <radhaya9av@gmail.com>
2024-07-28 21:48:59 +0530

    mealSingle

---
+++ src/components/Meal/MealSingle.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import "./Meal.scss";
+import { FaUtensilSpoon } from "react-icons/fa";
+import { AiFillHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { BiChevronsRight } from "react-icons/bi";
+import { AiOutlineCheckSquare } from 'react-icons/ai';
+
+const MealSingle = ({meal}) => {
+  console.log(meal);
+  let tags = meal?.tags?.split(',');
+  console.log(meal);
+  let instructions = meal?.instructions?.split('\r\n');
+  instructions = instructions?.filter(instruction => instruction.length > 1);
+
+  return (
+    <div className='section-wrapper'>
+      <div className='container'>
+        <div className='breadcrumb bg-orange text-white'>
+          <ul className='flex align-center my-2'>
+            <li className='breadcrumb-item'>
+              <Link to = "/" className='flex align-center'>
+                <AiFillHome size = {22} />
+              </Link>
+            </li>
+            <li className='flex align-center mx-1'>
+              <BiChevronsRight size = {23} />
+            </li>
+            <li className='breadcrumb-item flex'>
+              <span to = "" className='fs-15 fw-5 text-uppercase'>{meal?.title}</span>
+            </li>
+          </ul>
+        </div>
+          <div className='details-body'>
+            <div className='measures my-4'>
+              <h6 className='fs-16'>Measure:</h6>
+              <ul className='grid'>
+                {
+                  meal?.measures?.map((measure, idx) => (
+                    <li key = {idx} className = "fs-14 flex align-end">
+                      <span className='li-icon fs-12 text-orange'>
+                        <FaUtensilSpoon />
+                      </span>
+                      <span className='li-text fs-15 fw-6 op-09'>{measure}</span>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+
+            <div className='instructions my-4'>
+              <h6 className='fs-16'>Instructions:</h6>
+              <ul className='grid'>
+                {
+                  instructions?.map((instruction, idx) => (
+                    <li key = {idx} className = "fs-14">
+                      <AiOutlineCheckSquare size = {18} className = "text-orange li-icon" />
+                      <span className='li-text fs-16 fw-5 op-09'>{instruction}</span>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default MealSingle
